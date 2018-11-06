5ef57b08079ae9f8d789ba671e5373e12b0d27c7
Neil Sood <soodorama@gmail.com>
2018-11-05 18:01:15 -0800

    playground 2&3

--- playground/playground.py
+++ playground/playground.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/play')
 def play():
-    return render_template("index.html", boxes = 3)
+    return render_template("index.html", boxes = 3, color="blue")
 
 @app.route('/play/<num>')
 def playx(num):
-    return render_template("index.html", boxes = int(num))
+    return render_template("index.html", boxes = int(num), color="blue")
+    
+@app.route('/play/<num>/<col>')
+def playxy(num, col):
+    return render_template("index.html", boxes = int(num), color=col)
 
 if __name__ == "__main__":
     app.run(debug=True)
